3c52473efc80c1f2b06a9b5f9a89b88f7d896004
GiangLe1999 <legiangbmt09@gmail.com>
2023-01-01 08:09:56 +0700

    1/1/2023: Build base for Project

---
+++ src/pages/Search/index.js
@@ -0,0 +1,10 @@
+function Search() {
+    return (
+        <div>
+            <div className="content">
+                <h2>Search</h2>
+            </div>
+        </div>
+    );
+}
+export default Search;
